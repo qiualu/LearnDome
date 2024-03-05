0463f494ce6d868bd36deb9eecc9621a04dd8958
qiualu <1101938064@qq.com>
2024-03-05 10:58:46 +0800

    first commit

---
+++ Assets/UnityBiliBili/L01/L03_Test.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class L03_Test : MonoBehaviour
+{
+    private void Awake()
+    {
+        Debug.Log("awake");
+    }
+
+    private void OnEnable() // 激活组件的时候
+    {
+        Debug.Log("OnEnable");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
