2c6ef66c62841cd9e07d0e97bcf19da4129f0ded
qiualu <1101938064@qq.com>
2024-03-12 18:16:08 +0800

    3.12

--- Assets/UnityBiliBili/L02_Scripts/L0203Empty.cs
+++ Assets/UnityBiliBili/L02_Scripts/L0203Empty.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class L0203Empty : MonoBehaviour
 {
+
+    public GameObject Cube;
+    // 获取预设体
+    public GameObject Prefab; 
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +22,38 @@ public class L0203Empty : MonoBehaviour
         Debug.Log(gameObject.name);
         // tag
         Debug.Log(gameObject.tag);
+        // layer 
+        Debug.Log(gameObject.layer);
+        // 立方体的名称
+        Debug.Log(Cube.name);
+        //Debug.Log(Cube.active); // 是否激活
+        //当前真正的激活状态
+        Debug.Log(Cube.activeInHierarchy); // 是否激活
+        // 本身是否激活状态
+        Debug.Log(Cube.activeSelf); // 是否激活
+        // 获取 Transform组件
+        // Transform trans = this.transform;
+        Debug.Log(transform.position);
+        // 获取其他组件
+        BoxCollider bc = GetComponent<BoxCollider>();
+        // 获取当前物体的子物体身上的某个组件
+        //GetComponentInParent<BoxCollider>();
+        //GetComponentInParent<BoxCollider>();
+        // 添加一个组件
+        //gameObject.AddComponent<AudioSource>();
+        Cube.AddComponent<AudioSource>();
+
+        // 通过游戏物体的名称来获取游戏物体
+        GameObject test = GameObject.Find("Test");
+        Debug.Log(test.name);
+        // 通过游戏标签来获取游戏物体
+        test = GameObject.FindWithTag("Enemy");
+        Debug.Log(test.name);
+
+        // 通过预设体来实例化一个游戏对象 Vector3(1.93, 1, 8.75)
+        Instantiate(Prefab, Vector3.one, Quaternion.identity);
+        // 销毁
+        //Destroy(go);
 
     }
 
